cf81e60bdbc0dd55a5546652ce0d23935cdb124d
robinson Roy <robinson.roy@me.com>
2016-10-28 17:33:32 +0200

    delete gitignore

---
+++ src/UnitTest/firstTest.java
@@ -0,0 +1,14 @@
+package UnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class firstTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
