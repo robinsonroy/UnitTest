0b62bf73e213de3e4139a360ff0141bcc1299da5
robinson Roy <robinson.roy@me.com>
2016-10-31 10:16:42 +0100

    git settup

---
+++ src/mediathequeTest/FicheEmpruntTest.java
@@ -0,0 +1,84 @@
+package mediathequeTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FicheEmpruntTest {
+
+	@Test
+	public void testFicheEmprunt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifierClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCorrespond() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestituer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDocument() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateEmprunt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateLimite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepasse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDureeEmprunt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTarifEmprunt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangementCategorie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfficherStatistiques() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/mediathequeTest/GenreTest.java
@@ -0,0 +1,54 @@
+package mediathequeTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GenreTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmprunter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNbEmprunts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfficherStatistiques() {
+		fail("Not yet implemented");
+	}
+
+}
