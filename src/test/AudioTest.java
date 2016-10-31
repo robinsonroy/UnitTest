2e7543bf3748fe5f1e18ea1825e40c5b302a4e87
Vivien <v.brunel@live.fr>
2016-10-31 11:37:28 +0100

    MAJ

--- src/test/AudioTest.java
+++ src/test/AudioTest.java
@@ -15,8 +15,17 @@ import mediatheque.document.Audio;
 
 public class AudioTest extends TestCase{
 	
+	private Audio audiotest;
+	
 	@Before
 	public void setUp (){
+		String stringNotNull = new String("blabla");
+		Genre genreNotNull = new Genre("test");
+		Localisation localisationNotNull = new Localisation("test", "test");
+		try {
+			audiotest = new Audio(stringNotNull, localisationNotNull, stringNotNull, stringNotNull, stringNotNull, genreNotNull, stringNotNull);
+		} catch (OperationImpossible e) {} 
+		  catch (InvariantBroken e) {}
 	}
 	
 	@Test(expected=OperationImpossible.class)
@@ -38,6 +47,33 @@ public class AudioTest extends TestCase{
 			fail("Operation Impossible Exception Expected");
 		} catch (OperationImpossible e) {} 
 		  catch (InvariantBroken e) {}
+	}
+	
+	@Test
+	public void testNotNullInstance(){
+		assertNotNull("Instance n'a pas été créée", audiotest);
+		
+	}
+	
+	@Test
+	public void testInstanciation(){
+		assertEquals("Problème d'instanciation des variables String", "blabla", audiotest.getAnnee());
+		assertEquals("Problème d'instanciation des variables String", "blabla", audiotest.getAuteur());
+		assertEquals("Problème d'instanciation des variables String", "blabla", audiotest.getCode());
+		assertEquals("Problème d'instanciation des variables String", "blabla", audiotest.getTitre());
+		assertEquals("Problème instanciation localisation", new Localisation("test","test"), audiotest.getLocalisation());
+		assertEquals("Problème instanciation du genre", new Genre("test"), audiotest.getGenre());
+		assertEquals("L'emprunt n'a pas été initialisé à zéro", 0, audiotest.getNbEmprunts());
+	}
+	
+	@Test
+	public void testEmprunter(){
+		try {
+			audiotest.emprunter();
+			assertEquals("Problème incrémentation nombre d'emprunts", 1, audiotest.getNbEmprunts());
+		} catch (InvariantBroken e) {} 
+		catch (OperationImpossible e) {}
+		
 	}
 
 }
