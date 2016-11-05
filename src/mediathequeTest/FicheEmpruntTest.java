efd72e5014d2451a2b57ac9b9fdace7bfdb82dbc
robinson Roy <robinson.roy@me.com>
2016-11-05 13:25:17 +0100

    add FicheEmpruntTest

---
+++ src/mediathequeTest/FicheEmpruntTest.java
@@ -0,0 +1,81 @@
+package mediathequeTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Robinson on 05/11/2016.
+ */
+public class FicheEmpruntTest {
+    @Test
+    public void verifier() throws Exception {
+
+    }
+
+    @Test
+    public void modifierClient() throws Exception {
+
+    }
+
+    @Test
+    public void correspond() throws Exception {
+
+    }
+
+    @Test
+    public void restituer() throws Exception {
+
+    }
+
+    @Test
+    public void getClient() throws Exception {
+
+    }
+
+    @Test
+    public void getDocument() throws Exception {
+
+    }
+
+    @Test
+    public void getDateEmprunt() throws Exception {
+
+    }
+
+    @Test
+    public void getDateLimite() throws Exception {
+
+    }
+
+    @Test
+    public void getDepasse() throws Exception {
+
+    }
+
+    @Test
+    public void getDureeEmprunt() throws Exception {
+
+    }
+
+    @Test
+    public void getTarifEmprunt() throws Exception {
+
+    }
+
+    @Test
+    public void changementCategorie() throws Exception {
+
+    }
+
+    @Test
+    public void TestToString() throws Exception {
+
+    }
+
+    @Test
+    public void afficherStatistiques() throws Exception {
+
+    }
+
+}
